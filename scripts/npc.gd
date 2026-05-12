811a384d06fa86d169b5ed489a3dfa7393f82068
Ethan Sasiela <esasiela@gmail.com>
2026-05-12 06:47:44 -0300

    Refactor npc into custom scenes instead of instances

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -23,4 +23,22 @@ func talk_to(player: Player) -> void:
 	if not dialogue:
 		return
 	var dialogue_ui := get_tree().get_first_node_in_group("dialogue_ui") as DialogueUI
+	dialogue_ui.choice_selected.connect(_on_dialogue_choice)
+	dialogue_ui.closed.connect(_on_dialogue_closed)
 	dialogue_ui.show_dialogue(dialogue, self)
+
+
+func _on_dialogue_choice(action: String) -> void:
+	match action:
+		"open_vendor":
+			_open_vendor()
+
+
+func _on_dialogue_closed() -> void:
+	var dialogue_ui := get_tree().get_first_node_in_group("dialogue_ui") as DialogueUI
+	dialogue_ui.choice_selected.disconnect(_on_dialogue_choice)
+	dialogue_ui.closed.disconnect(_on_dialogue_closed)
+
+
+func _open_vendor() -> void:
+	print(display_name, " would open vendor here")
